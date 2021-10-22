e7e15b362454b9c87ef57831d581ed7b8695276f
MortalHappiness <b07901069@ntu.edu.tw>
2021-10-22 17:25:57 +0800

    Finish touch point capture

--- FTIR_VideoCapture.py
+++ FTIR_VideoCapture.py
@@ -9,50 +9,58 @@ cap = cv2.VideoCapture(1)
 while(True):
 	# Capture one frame from the camera
 	ret, frame = cap.read()
+	frame = cv2.flip(frame, 1)
 
 	# Split RGB channels
 	B, G, R = cv2.split(frame)
 
 	# Perform thresholding to each channel
-	#_, R = cv2.threshold(R, 130, 255, cv2.THRESH_BINARY)
-	#cv2.imshow('frame', R)
-	_, G = cv2.threshold(G, 100, 255, cv2.THRESH_BINARY)
-	_, B = cv2.threshold(B, 110, 255, cv2.THRESH_BINARY_INV)
-	#cv2.imshow('frame', B)
+	_, R = cv2.threshold(R, 120, 255, cv2.THRESH_BINARY)
+	# _, G = cv2.threshold(G, 100, 255, cv2.THRESH_BINARY_INV)
+	_, B = cv2.threshold(B, 90, 255, cv2.THRESH_BINARY_INV)
 
 	# Get final result using bitwise operation
-	Resb = cv2.bitwise_and(G, B, mask=None)
-	#cv2.imshow('frame', Result)
+	Display = cv2.bitwise_and(R, B, mask=None)
 
-	# blur and then threshold again?
-	#Resb = cv2.blur(Result, (5, 5))
-	#_, Resb = cv2.threshold(blurred, 140, 255, cv2.THRESH_BINARY)
+	# Blur
+	Display = cv2.blur(Display, (15, 15))
+	_, Display = cv2.threshold(Display, 90, 255, cv2.THRESH_BINARY)
 
 	# Find and draw contours
-	contour, hierarchy = cv2.findContours(Resb, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	contour, hierarchy = cv2.findContours(Display, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 	## for debug
-	Display = cv2.cvtColor(Resb, cv2.COLOR_GRAY2BGR)
+	Display = cv2.cvtColor(Display, cv2.COLOR_GRAY2BGR)
 	cv2.drawContours(Display, contour, -1, (0, 0, 255))
-	print([cv2.contourArea(cnt) for cnt in contour])
 
 	# Iterate through each contours, check area and find center
-	Display = cv2.flip(Display, 1)
-	for cnt in contour : 
+	for cnt in contour: 
 		area = cv2.contourArea(cnt)
 		M = cv2.moments(cnt)
-		if M['m00'] != 0 and area > 10000:
-			x = M['m10']/M['m00']
-			y = M['m01']/M['m00']
-			pos = str(int(x))+str(int(y))
-			cv2.putText(Display, pos, (int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
-			Display = cv2.circle(Display, (int(x),int(y)), radius=10, color=(0, 0, 255), thickness=-1)
+		if M['m00'] != 0 and area > 7000:
+			x = int(M['m10']/M['m00'])
+			y = int(M['m01']/M['m00'])
+			cv2.putText(Display, f"{area}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255))
+			Display = cv2.circle(Display, (x, y), radius=10, color=(0, 0, 255), thickness=-1)
 
 	# Show image
-	#cv2.imshow('frame', frame)
-	st = np.hstack((R, B, G, Resb))
-	cv2.imshow('frame', st)
-	#cv2.imshow('frame', Display)
+	debug = True
+	if debug:
+		Display = cv2.hconcat([frame, Display, np.zeros_like(frame)])
+		Display = cv2.vconcat([Display, cv2.hconcat([cv2.cvtColor(x, cv2.COLOR_GRAY2BGR) for x in [R, G, B]])])
+		zeros = np.zeros_like(R)
+		R = cv2.merge([zeros, zeros, R])
+		G = cv2.merge([zeros, G, zeros])
+		B = cv2.merge([B, zeros, zeros])
+		Display = cv2.vconcat([Display, cv2.hconcat([R, G, B])])
+		resize_scale = 0.5
+		scale_percent = 60
+		width = int(Display.shape[1] * scale_percent / 100)
+		height = int(Display.shape[0] * scale_percent / 100)
+		dim = (width, height)
+		Display = cv2.resize(Display, dim)
+
+	cv2.imshow('frame', Display)
 
 	# Press q to quit
 	if cv2.waitKey(1) & 0xFF == ord('q'):
