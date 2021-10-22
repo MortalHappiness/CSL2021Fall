9316314f04e708a9714250027b101ff178d45051
MortalHappiness <b07901069@ntu.edu.tw>
2021-10-22 18:58:27 +0800

    Finish FTIR except for predict

--- FTIR_VideoCapture.py
+++ FTIR_VideoCapture.py
@@ -6,6 +6,19 @@ import matplotlib.pyplot as plt
 # cap = cv2.VideoCapture(0)
 cap = cv2.VideoCapture(1)
 
+text = ""
+
+pts = np.zeros(shape=(0, 1, 2), dtype=np.int32)
+LEAVE_TIMEOUT = 50
+ENTER_TIMEOUT = 30
+DISPLAY_TIMEOUT = 50
+leave_timeout = 0
+enter_timeout = 0
+display_timeout = 0
+
+def predict(img, points):
+	return 8
+
 while(True):
 	# Capture one frame from the camera
 	ret, frame = cap.read()
@@ -34,17 +47,41 @@ while(True):
 	cv2.drawContours(Display, contour, -1, (0, 0, 255))
 
 	# Iterate through each contours, check area and find center
+	x, y = None, None
 	for cnt in contour: 
 		area = cv2.contourArea(cnt)
 		M = cv2.moments(cnt)
 		if M['m00'] != 0 and area > 7000:
-			x = int(M['m10']/M['m00'])
-			y = int(M['m01']/M['m00'])
-			cv2.putText(Display, f"{area}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255))
-			Display = cv2.circle(Display, (x, y), radius=10, color=(0, 0, 255), thickness=-1)
+			x, y = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
+
+	if x is not None and y is not None:
+		leave_timeout = LEAVE_TIMEOUT
+		display_timeout = 0
+		enter_timeout += 1
+		cv2.putText(Display, f"{area}", (x, y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255))
+		cv2.circle(Display, (x, y), radius=10, color=(0, 0, 255), thickness=-1)
+
+		if enter_timeout >= ENTER_TIMEOUT:
+			if pts.shape[0] == 0 or not (pts[-1, 0, 0] == x and pts[-1, 0, 1] == y):
+				pts = np.concatenate((pts, np.array([x, y]).reshape((-1, 1, 2))))
+	else:
+		enter_timeout = 0
+
+	if leave_timeout > 0:
+		leave_timeout -= 1
+		cv2.polylines(Display, [pts], False, (255, 255, 0), 4)
+	elif pts.shape != (0, 1, 2):
+		# Predict number
+		text = str(predict(Display, pts))
+		pts = np.zeros(shape=(0, 1, 2), dtype=np.int32)
+		display_timeout = DISPLAY_TIMEOUT
+	
+	if display_timeout > 0:
+		display_timeout -= 1
+		cv2.putText(Display, text, (200, 400), cv2.FONT_HERSHEY_SIMPLEX, 15, (0, 255, 255), 20)
 
 	# Show image
-	debug = True
+	debug = False
 	if debug:
 		Display = cv2.hconcat([frame, Display, np.zeros_like(frame)])
 		Display = cv2.vconcat([Display, cv2.hconcat([cv2.cvtColor(x, cv2.COLOR_GRAY2BGR) for x in [R, G, B]])])
